060583df0fe258bf1dd5811a270c7c76ed63336d
KANGPUNGYUN <zkdvnd@naver.com>
2023-09-18 23:49:53 +0900

    feat: signup page

--- src/app/sign_up/form/index.tsx
+++ src/app/sign_up/form/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useForm } from "react-hook-form";
-import { faCaretDown, faAngleRight } from "@fortawesome/free-solid-svg-icons";
+import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 
@@ -9,17 +9,34 @@ interface FormInput {
   password: string;
   confirmPassword: string;
   nickname: string;
+  agreeAll: boolean;
+  agree1: boolean;
+  agree2: boolean;
+  agree3: boolean;
 }
+
+let renderCount = 0;
 
 export default function SignUpForm() {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<FormInput>();
+    getValues,
+    watch,
+  } = useForm<FormInput>({
+    mode: "onChange",
+  });
 
+  const agreeAll = watch("agreeAll");
+
+  renderCount++;
   return (
     <>
+      <link
+        href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        rel="stylesheet"
+      />
       <form
         className="p-signup-form"
         onSubmit={handleSubmit((data) => {
@@ -27,7 +44,15 @@ export default function SignUpForm() {
         })}
       >
         <div className="p-signup-form-email-input__outer">
-          <label className="p-signup-form-label">이메일</label>
+          <label
+            className={
+              errors.email
+                ? "p-signup-form-label errored"
+                : "p-signup-form-label"
+            }
+          >
+            이메일
+          </label>
           <div className="p-signup-form-email-input">
             <div className="p-signup-form-email-input__inner">
               <span className="p-signup-form-email-input__local">
@@ -35,42 +60,21 @@ export default function SignUpForm() {
                   <input
                     {...register("email", {
                       required: "필수 입력 항목입니다.",
+                      pattern: {
+                        value:
+                          /^[a-zA-Z0-9+-\_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+                        message: "이메일 형식이 올바르지 않습니다.",
+                      },
                     })}
-                    className="form-control"
+                    className={
+                      errors.email ? "form-control errored" : "form-control"
+                    }
                     placeholder="이메일"
+                    autoComplete="off"
                   />
                   <span className="form-control-blind">
                     이메일 앞 주소 입력하기
                   </span>
-                </label>
-              </span>
-              <span className="email-input__separator">@</span>
-              <span className="p-signup-form-email-input__domain">
-                <label>
-                  <select className="form-control webkit-none">
-                    <option>선택해주세요</option>
-                    <option value="naver.com">naver.com</option>
-                    <option value="hanmail.net">hanmail.net</option>
-                    <option value="daum.net">daum.net</option>
-                    <option value="gmail.com">gmail.com</option>
-                    <option value="hotmail.com">hotmail.com</option>
-                    <option value="_manual">직접입력</option>
-                  </select>
-                  <span className="form-control-blind">
-                    이메일 도메인 선택하기
-                  </span>
-                  <button
-                    className="email-input__domain__expand"
-                    aria-label="초기화"
-                    type="button"
-                    tabIndex={-1}
-                  >
-                    <FontAwesomeIcon
-                      icon={faCaretDown}
-                      width="10px"
-                      height="10px"
-                    />
-                  </button>
                 </label>
               </span>
             </div>
@@ -89,19 +93,33 @@ export default function SignUpForm() {
           </button>
         </div>
         <div className="p-signup-form-password-input__outer">
-          <label className="p-signup-form-label">비밀번호</label>
+          <label
+            className={
+              errors.password
+                ? "p-signup-form-label errored"
+                : "p-signup-form-label"
+            }
+          >
+            비밀번호
+          </label>
           <div className="p-signup-form-description">
-            영문, 숫자를 포함한 8자 이상, 25자 이하의 비밀번호를 입력해주세요.
+            영문, 숫자를 포함한 8자 이상의 비밀번호를 입력해주세요.
           </div>
           <label>
             <input
               {...register("password", {
                 required: "필수 입력 항목입니다.",
-                minLength: 8,
-                maxLength: 25,
-                pattern: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,25}$/,
+                pattern: {
+                  value: /^(?=.*?[A-Za-z])(?=.*?[0-9]).{8,}$/,
+                  message:
+                    "비밀번호는 영문, 숫자를 포함하여 8자 이상이어야 합니다.",
+                },
               })}
-              className="p-signup-form-password-input"
+              className={
+                errors.password
+                  ? "p-signup-form-password-input errored"
+                  : "p-signup-form-password-input"
+              }
               type="password"
               name="password"
               placeholder="비밀번호"
@@ -113,13 +131,32 @@ export default function SignUpForm() {
           </div>
         </div>
         <div className="p-signup-form-confirm-password-input__outer">
-          <label className="p-signup-form-label">비밀번호 확인</label>
+          <label
+            className={
+              errors.confirmPassword
+                ? "p-signup-form-label errored"
+                : "p-signup-form-label"
+            }
+          >
+            비밀번호 확인
+          </label>
           <label>
             <input
               {...register("confirmPassword", {
                 required: "확인을 위해 비밀번호를 한 번 더 입력해주세요.",
+                validate: {
+                  check: (val) => {
+                    if (getValues("password") !== val) {
+                      return "비밀번호가 일치하지 않습니다.";
+                    }
+                  },
+                },
               })}
-              className="p-signup-form-confirm-password-input"
+              className={
+                errors.confirmPassword
+                  ? "p-signup-form-confirm-password-input errored"
+                  : "p-signup-form-confirm-password-input"
+              }
               type="password"
               name="confirmPassword"
               placeholder="비밀번호 확인"
@@ -131,17 +168,38 @@ export default function SignUpForm() {
           </div>
         </div>
         <div className="p-signup-form-nickname-input__outer">
-          <label className="p-signup-form-label">닉네임</label>
+          <label
+            className={
+              errors.nickname
+                ? "p-signup-form-label errored"
+                : "p-signup-form-label"
+            }
+          >
+            닉네임
+          </label>
           <label>
             <input
               {...register("nickname", {
                 required: "필수 입력 항목입니다.",
-                minLength: 2,
-                maxLength: 15,
+                minLength: { value: 2, message: "2자 이상으로 입력해주세요." },
+                maxLength: { value: 15, message: "15자 이하로 입력해주세요." },
+                // 추가 작업
+                // validate: {
+                //   check: (val) => {
+                //     if (!!getNickname(val)) {
+                //       return "사용 중인 별명입니다.";
+                //     }
+                //   },
+                // },
               })}
-              className="p-signup-form-nickname-input"
+              className={
+                errors.nickname
+                  ? "p-signup-form-nickname-input errored"
+                  : "p-signup-form-nickname-input"
+              }
               name="nickname"
               placeholder="별명 (2~15자)"
+              autoComplete="off"
             />
             <span className="form-control-blind">닉네임 입력하기</span>
           </label>
@@ -154,14 +212,25 @@ export default function SignUpForm() {
           <div className="p-signup-form-ToS-container">
             <div className="p-signup-form-ToS-agreeAll-label__outer">
               <label className="p-signup-form-ToS-agreeAll-label">
-                <div className="p-signup-form-ToS-agreeAll-input__outer">
+                <div
+                  className={
+                    errors.agreeAll
+                      ? "p-signup-form-ToS-agreeAll-input__outer"
+                      : "p-signup-form-ToS-agreeAll-input__outer active"
+                  }
+                >
                   <div className="p-signup-form-ToS-agreeAll-checkbox">
                     <span className="p-signup-form-ToS-agreeAll-check"></span>
                   </div>
                   <input
                     className="p-signup-form-ToS-agreeAll-input"
                     type="checkbox"
-                    name="agreeAll"
+                    {...register("agreeAll", {
+                      required: {
+                        value: true,
+                        message: "필수 항목에 동의해주세요.",
+                      },
+                    })}
                   />
                 </div>
                 <span className="p-signup-form-ToS-agreeAll-title__outer">
@@ -174,14 +243,26 @@ export default function SignUpForm() {
             <hr className="p-signup-form-ToS-container-hr" />
             <div className="p-signup-form-ToS-agree1-label__outer">
               <label className="p-signup-form-ToS-agree1-label">
-                <div className="p-signup-form-ToS-agree1-input__outer">
+                <div
+                  className={
+                    errors.agree1
+                      ? "p-signup-form-ToS-agree1-input__outer"
+                      : "p-signup-form-ToS-agree1-input__outer active"
+                  }
+                >
                   <div className="p-signup-form-ToS-agree1-checkbox">
                     <span className="p-signup-form-ToS-agree1-check"></span>
                   </div>
                   <input
                     className="p-signup-form-ToS-agree1-input"
                     type="checkbox"
-                    name="agree1"
+                    checked={agreeAll}
+                    {...register("agree1", {
+                      required: {
+                        value: true,
+                        message: "필수 항목에 동의해주세요.",
+                      },
+                    })}
                   />
                 </div>
                 <span className="p-signup-form-ToS-agree1-title__outer">
@@ -193,14 +274,26 @@ export default function SignUpForm() {
             </div>
             <div className="p-signup-form-ToS-agree2-label__outer">
               <label className="p-signup-form-ToS-agree2-label">
-                <div className="p-signup-form-ToS-agree2-input__outer">
+                <div
+                  className={
+                    errors.agree2
+                      ? "p-signup-form-ToS-agree2-input__outer"
+                      : "p-signup-form-ToS-agree2-input__outer active"
+                  }
+                >
                   <div className="p-signup-form-ToS-agree2-checkbox">
                     <span className="p-signup-form-ToS-agree2-check"></span>
                   </div>
                   <input
                     className="p-signup-form-ToS-agree2-input"
                     type="checkbox"
-                    name="agree2"
+                    checked={agreeAll}
+                    {...register("agree2", {
+                      required: {
+                        value: true,
+                        message: "필수 항목에 동의해주세요.",
+                      },
+                    })}
                   />
                 </div>
                 <span className="p-signup-form-ToS-agree2-title__outer">
@@ -215,14 +308,26 @@ export default function SignUpForm() {
             </div>
             <div className="p-signup-form-ToS-agree3-label__outer">
               <label className="p-signup-form-ToS-agree3-label">
-                <div className="p-signup-form-ToS-agree3-input__outer">
+                <div
+                  className={
+                    errors.agree3
+                      ? "p-signup-form-ToS-agree3-input__outer"
+                      : "p-signup-form-ToS-agree3-input__outer active"
+                  }
+                >
                   <div className="p-signup-form-ToS-agree3-checkbox">
                     <span className="p-signup-form-ToS-agree3-check"></span>
                   </div>
                   <input
                     className="p-signup-form-ToS-agree3-input"
                     type="checkbox"
-                    name="agree3"
+                    checked={agreeAll}
+                    {...register("agree3", {
+                      required: {
+                        value: true,
+                        message: "필수 항목에 동의해주세요.",
+                      },
+                    })}
                   />
                 </div>
                 <span className="p-signup-form-ToS-agree3-title__outer">
