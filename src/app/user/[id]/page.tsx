4b036a6b8f952367525210c8a11bdef9c803e2dd
KANGPUNGYUN <zkdvnd@naver.com>
2023-10-06 21:12:54 +0900

    init: user

---
+++ src/app/user/[id]/page.tsx
@@ -0,0 +1,14 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "하루쓰기 | 나의 하루",
+  description: "당신의 하루를 작성하세요",
+};
+
+export default function MyDiary() {
+  return (
+    <>
+      <main></main>
+    </>
+  );
+}
---
+++ src/app/user/page.tsx
@@ -0,0 +1,14 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "하루쓰기 | 모두의 하루",
+  description: "당신의 하루를 작성하세요",
+};
+
+export default function User() {
+  return (
+    <>
+      <main></main>
+    </>
+  );
+}
