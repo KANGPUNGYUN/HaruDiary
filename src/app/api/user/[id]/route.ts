54c3256812aa1a758840f447d3c68c77d59be122
KANGPUNGYUN <zkdvnd@naver.com>
2023-10-27 15:24:09 +0900

    example: vercel postgres

--- src/app/api/user/[id]/route.ts
+++ src/app/api/user/[id]/route.ts
@@ -1,39 +1,55 @@
 // import { verifyJwt } from "@/app/lib/jwt";
-import prisma from "@/app/lib/prisma";
+// import prisma from "@/app/lib/prisma";
 
-export async function GET(
+// export async function GET(
+//   request: Request,
+//   { params }: { params: { id: string } }
+// ) {
+//   const accessToken = request.headers.get("authorization");
+// if (!accessToken || !verifyJwt(accessToken)) {
+//   console.log(accessToken);
+//   return new Response(JSON.stringify({ error: "No Authorization" }), {
+//     status: 401,
+//   });
+// }
+
+//   console.log(params);
+
+//   const id = Number(params.id);
+
+//   const userPosts = await prisma.diary.findMany({
+//     where: {
+//       authorId: id,
+//     },
+//     include: {
+//       author: {
+//         select: {
+//           email: true,
+//           name: true,
+//         },
+//       },
+//     },
+//     orderBy: [
+//       {
+//         id: "desc",
+//       },
+//     ],
+//   });
+//   return new Response(JSON.stringify(userPosts));
+// }
+
+import { db } from "@vercel/postgres";
+
+export default async function handler(
   request: Request,
   { params }: { params: { id: string } }
 ) {
-  const accessToken = request.headers.get("authorization");
-  // if (!accessToken || !verifyJwt(accessToken)) {
-  //   console.log(accessToken);
-  //   return new Response(JSON.stringify({ error: "No Authorization" }), {
-  //     status: 401,
-  //   });
-  // }
+  const client = await db.connect();
 
-  console.log(params);
-
-  const id = Number(params.id);
-
-  const userPosts = await prisma.diary.findMany({
-    where: {
-      authorId: id,
-    },
-    include: {
-      author: {
-        select: {
-          email: true,
-          name: true,
-        },
-      },
-    },
-    orderBy: [
-      {
-        id: "desc",
-      },
-    ],
-  });
-  return new Response(JSON.stringify(userPosts));
+  try {
+    const user = await client.sql`SELECT * FROM user;`;
+    return new Response(JSON.stringify(user));
+  } catch (error) {
+    console.log(error);
+  }
 }
