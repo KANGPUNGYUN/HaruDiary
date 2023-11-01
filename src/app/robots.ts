7f31669e9cfdb68c0889dea2cebef8c8bdc7af6a
KANGPUNGYUN <zkdvnd@naver.com>
2023-11-01 19:20:21 +0900

    seo: sitemap

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -3,9 +3,10 @@ import { MetadataRoute } from "next";
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
-      userAgent: ["Yeti", "Googlebot"],
+      userAgent: "*",
       allow: "/",
       disallow: "/user/",
     },
+    sitemap: "https://harudiary.vercel.app/sitemap.xml",
   };
 }
