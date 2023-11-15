47d8cd70ebb18b49932aea2ab03645dd29fa0fe5
KANGPUNGYUN <zkdvnd@naver.com>
2023-11-15 17:22:27 +0900

    SEO: add naver meta link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,11 +19,7 @@ export default function Home() {
     <>
       <meta
         name="naver-site-verification"
-        content="0fc0efcb926dc4118a917eeef06099c48d49a4fc"
-      />
-      <meta
-        name="google-site-verification"
-        content="cHTy20JNIadsj7DMtxa2d5dHarTPhGW2tTv9UO7UEu8"
+        content="f4fcaf599f0dcc374e49eddfcf8535cbc666655f"
       />
       <main>
         <section className="p-home-section section1">
