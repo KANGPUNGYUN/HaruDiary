ae6bce68c255ac98048a3428b5f02da279849493
KANGPUNGYUN <zkdvnd@naver.com>
2023-12-01 13:09:42 +0900

    refactoring: change sitemap file

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -8,11 +8,5 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "yearly",
       priority: 1,
     },
-    {
-      url: "https://www.harudiary.site",
-      lastModified: new Date(),
-      changeFrequency: "yearly",
-      priority: 0.5,
-    },
   ];
 }
